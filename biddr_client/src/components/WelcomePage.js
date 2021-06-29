425a3d63e4d951309f3347f13eb16b4b79dbf89d
Sree <vjsreekanth@gmail.com>
2021-06-28 20:22:43 -0700

    updated welcome page

--- biddr_client/src/components/WelcomePage.js
+++ biddr_client/src/components/WelcomePage.js
@@ -3,6 +3,7 @@ import auction from '../auction.jpg';
 const WelcomePage = () => {
     return(
         <div>
+            <h2>Welcome to Biddr App</h2>
             <img src={ auction } alt="auction"/>
         </div>
         
